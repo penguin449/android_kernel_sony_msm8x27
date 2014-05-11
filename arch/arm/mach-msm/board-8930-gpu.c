4d9b3720d3d24122be2df4bd3876cdd769290de0
Arnav Gosain <nolinuxnoparty@gmail.com>
2014-05-11 04:35:28 -0400

    Revert "nicki: gpu: overclock to 533Mhz"
    This reverts commit cee226ad8f3d8515c9aef59fa564540538e18fbb.

--- arch/arm/mach-msm/board-8930-gpu.c
+++ arch/arm/mach-msm/board-8930-gpu.c
@@ -40,28 +40,6 @@ static struct msm_bus_vectors grp3d_low_vectors[] = {
 	},
 };
 
-#ifdef CONFIG_GPU_OVERCLOCK
-
-static struct msm_bus_vectors grp3d_nominal_vectors[] = {
-	{
-		.src = MSM_BUS_MASTER_GRAPHICS_3D,
-		.dst = MSM_BUS_SLAVE_EBI_CH0,
-		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(4264),
-	},
-};
-
-static struct msm_bus_vectors grp3d_max_vectors[] = {
-	{
-		.src = MSM_BUS_MASTER_GRAPHICS_3D,
-		.dst = MSM_BUS_SLAVE_EBI_CH0,
-		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(5290),
-	},
-};
-
-#else
-
 static struct msm_bus_vectors grp3d_nominal_vectors[] = {
 	{
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
@@ -79,8 +57,6 @@ static struct msm_bus_vectors grp3d_max_vectors[] = {
 		.ib = KGSL_CONVERT_TO_MBPS(4264),
 	},
 };
-
-#endif
 
 static struct msm_bus_paths grp3d_bus_scale_usecases[] = {
 	{
@@ -139,29 +115,6 @@ static struct kgsl_device_iommu_data kgsl_3d0_iommu_data[] = {
 
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
-	
-#ifdef CONFIG_GPU_OVERCLOCK
-		{
- 			.gpu_freq = 533333000,
- 			.bus_freq = 5,
- 			.io_fraction = 0,
- 		},
-        {
- 			.gpu_freq = 487500000,
- 			.bus_freq = 4,
- 			.io_fraction = 0,
- 		},
-        {
- 			.gpu_freq = 400000000,
- 			.bus_freq = 3,
- 			.io_fraction = 0,
- 		},
- 		{
- 			.gpu_freq = 320000000,
- 			.bus_freq = 2,
- 			.io_fraction = 0,
- 		},
-#else
 		{
 			.gpu_freq = 400000000,
 			.bus_freq = 3,
@@ -172,7 +125,6 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.bus_freq = 2,
 			.io_fraction = 33,
 		},
-#endif
 		{
 			.gpu_freq = 192000000,
 			.bus_freq = 1,
@@ -184,7 +136,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		},
 	},
 	.init_level = 1,
-	.num_levels = 6,
+	.num_levels = 4,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/12,
 	.nap_allowed = true,
